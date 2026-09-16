8d0fa6032c077b8fc10e4e279c6e32b2490f284d
sofiaimf <sofiaisabelmf01@gmail.com>
2026-09-15 23:26:42 -0400

    Delete Robotic_Experiments directory

--- Robotic_Experiments/mutual_motion_camouflage/start_experiment.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-source /opt/ros/humble/setup.bash
-source "$HOME/turtlebot3_ws/install/setup.bash"
-export ROS_DOMAIN_ID="${ROS_DOMAIN_ID:-30}"
-export ROS_LOCALHOST_ONLY=0
-exec python3 "$script_dir/start_mmc_experiment.py" "$@"
